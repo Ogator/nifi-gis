0fb1cdf43314b174f22f7e2f642374a58276a8a3
Oleg Mironenko <oleg.dev.m@gmail.com>
2021-09-25 15:27:14 +0300

    fix deploy source path after changing folder structure

--- nifi-gis-processors-bundle/deploy_local.sh
+++ nifi-gis-processors-bundle/deploy_local.sh
@@ -12,6 +12,6 @@ NIFI_HOME=$NIFI_ROOT/nifi-assembly/target/nifi-${NIFI_VERSION_DEBUG}-bin/nifi-${
 # build the package
 mvn clean package -DskipTests
 # copy new NAR to debug location
-cp nifi-gis-nar/target/nifi-gis-processors-nar-${NAR_VERSION}.nar ${NIFI_HOME}/lib
+cp nifi-gis-processors-nar/target/nifi-gis-processors-nar-${NAR_VERSION}.nar ${NIFI_HOME}/lib
 # restart nifi
 ${NIFI_HOME}/bin/nifi.sh restart --wait-for-init
